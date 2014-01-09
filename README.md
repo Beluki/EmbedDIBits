84230460f4ba1d1bf8a78c3ebe92ec5c6bd71953
Beluki <beluki@gmx.com>
2014-01-09 18:29:22 +0100

    Added latest version.

--- README.md
+++ README.md
@@ -1,4 +1,123 @@
-EmbedDIBits
-===========
 
-Convert images to Win32 DIB byte arrays.
+### About
+
+EmbedDIBits is a small Python 3 program that can convert images to C
+byte arrays. The output is compatible with the Win32 API functions:
+[SetDIBits][], [StretchDIBits][], [AlphaBlend][] etc...
+
+This is useful in 4kb or 64kb demos, where it isn't possible link against
+libc or to use resource files with [Crinkler][] or as a general method
+to embed images without dealing with image formats complexity.
+
+### Installation and usage
+
+To install, just make sure you are using Python 3 and have the [Pillow][]
+module installed. EmbedDIBits is a single Python script that you can put
+in your PATH.
+
+Let's say we have an image named: 'sprite.bmp', living in the current
+folder. To generate the 'sprite.h' header:
+
+```bash
+$ EmbedDIBits.py sprite.bmp
+sprite.bmp -> sprite.h
+```
+
+And that's it. Now, from C:
+
+```c
+// include the generated header:
+#include "sprite.h"
+
+// declare a BITMAPINFO with the same width/height as the sprite
+// using 32bit depth:
+static const BITMAPINFO sprbmi = {
+    { sizeof(BITMAPINFOHEADER), WIDTH, -HEIGHT, 1, 32, BI_RGB, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0 }
+};
+
+// draw it (the variable is the image file basename without extension):
+StretchDIBits(..., sprite, &sprbmi, DIB_RGB_COLORS, SRCCOPY);
+```
+
+The 32 bit format is used because it's a lot easier to deal with sprites
+in the same format, it's the most general format and it's compatible with
+[AlphaBlend][], allowing for per-pixel alpha. Crinkler will compress
+the images anyway, so it doesn't matter. Formats supported are BMP, JPG,
+PNG, TGA, TIFF... anything supported by Pillow itself.
+
+### Command-line options
+
+EmbedDIBits has some options that can be used to change the behavior:
+
+* `--newline [dos, mac, unix, system]` changes the newline format.
+  I tend to use Unix newlines everywhere, even on Windows. The default is
+  `system`, which uses the current platform newline format.
+
+* `--no-buffer` makes it write directly to the target files. This is about
+  50% slower, but it uses less memory. Useful on huge images and on devices
+  that don't have much memory.
+
+* `--quiet` supresses the `sprite.bmp -> sprite.h` messages during conversion.
+  Errors will still be printed to stderr. Useful for scripts.
+
+* `--stdout` writes the generated code to standard output. It can be used
+  to generate a single header with all the sprites:
+
+  ```bash
+  $ EmbedDIBits.py --stdout *.png > sprites.h
+  (sprites.h is generated, errors go to stderr)
+  ```
+
+### Limitations
+
+Since EmbedDIBits chooses variable names based on the input image filenames,
+it's possible to produce incorrect C identifiers in some corner cases, for
+example when using reserved C keywords as filenames.
+
+I believe the convenience of just using 'sprite' in the common case, outweights
+the complexity of doing any kind of name-mangling for the corner cases.
+
+### Portability
+
+Information and error messages are written to stdout and stderr
+respectively, using the current platform newline format and UTF-8 encoding.
+
+Generated output (the C code) is written as UTF-8, using the newline format
+specified by the option `--newline ...`. By default, platform newlines are used
+because byte by byte equality of generated files is not important.
+
+The exit status is 0 on success and 1 on errors. After an error,
+EmbedDIBits skips the current file and proceeds with the next one
+instead of aborting. It can be interrupted with Control + C.
+
+EmbedDIBits is tested on Windows 7 and 8 and on Debian (both x86 and x86-64)
+using Python 3.3+ and Pillow 2.1.0+. Python 2.x is not supported.
+
+### Alternatives
+
+There are other programs (notably [GIMP][]) that can export to C. Unfortunately,
+I'm not aware of any program whose output is directly usable by StretchDIBits
+and the other Win32 API functions.
+
+### Status
+
+This program is feature-complete and has no known bugs. Unless new issues
+are reported or requests are made I plan no further development on it other
+than maintenance.
+
+### License
+
+Like all my hobby projects, this is Free Software. See the [Documentation][]
+folder for more information. No warranty though.
+
+[AlphaBlend]: http://msdn.microsoft.com/en-us/library/windows/desktop/dd183351%28v=vs.85%29.aspx
+[SetDIBits]: http://msdn.microsoft.com/en-us/library/windows/desktop/dd162973%28v=vs.85%29.aspx
+[StretchDIBits]: http://msdn.microsoft.com/en-us/library/windows/desktop/dd145121%28v=vs.85%29.aspx
+
+[Crinkler]: http://www.crinkler.net
+[GIMP]: http://www.gimp.org
+[Pillow]: https://pypi.python.org/pypi/Pillow
+
+[Documentation]: https://github.com/Beluki/EmbedDIBits/tree/master/Documentation
+
---
+++ Source/EmbedDIBits.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+EmbedDIBits.
+Convert images to Win32 DIB byte arrays.
+"""
+
+
+import io
+import os
+import sys
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+
+# Information and error messages:
+
+def outln(line):
+    """ Write 'line' to stdout, using UTF-8 and platform newline. """
+    print(line)
+
+
+def errln(line):
+    """ Write 'line' to stderr, using UTF-8 and platform newline. """
+    print(line, file = sys.stderr)
+
+
+# IO utils:
+
+def noext(filepath):
+    """ Return 'filepath' without the file extension. """
+    return os.path.splitext(filepath)[0]
+
+
+def noext_basename(filepath):
+    """ Return 'filepath' basename without the file extension. """
+    return noext(os.path.basename(filepath))
+
+
+def utf8_bytes(string):
+    """ Convert 'string' to bytes using UTF-8. """
+    return bytes(string, 'UTF-8')
+
+
+# Non-builtin imports:
+
+try:
+    from PIL import Image
+
+except ImportError:
+    errln('EmbedDIBits requires the following modules:')
+    errln('Pillow 2.0+ - <https://pypi.python.org/pypi/Pillow>')
+    sys.exit(1)
+
+
+# For portability, all C output is done in bytes
+# to avoid Python automatic newline conversion.
+
+
+# Since there are only 256 possible pixel R, G, B values
+# we cache all the conversions to hexadecimal values, as bytes.
+# e.g. BYTES_RGB_HEX[15] = b'0F'
+
+BYTES_RGB_HEX = [utf8_bytes(('%X' % number).rjust(2, '0')) for number in range(256)]
+
+
+# Possible newline options, as bytes:
+
+BYTES_NEWLINES = {
+    'dos'    : b'\r\n',
+    'mac'    : b'\r',
+    'unix'   : b'\n',
+    'system' : utf8_bytes(os.linesep),
+}
+
+
+def dibits2h(image, buffer, variable, newline):
+    """
+    Convert a Pillow 'image' to a C byte array, using 'variable' (as UTF-8)
+    for declaration and 'newline' format (as bytes).
+
+    The 'buffer' can be anything that implements a write() method,
+    such as a io.BytesIO() instance or a file descriptor,
+    as long as it accepts bytes.
+    """
+    # ensure that there is an alpha channel:
+    if image.mode != 'RGBA':
+        image = image.convert('RGBA')
+
+    width, height = image.size
+    total = width * height
+    pixels = image.load()
+
+    # emit declaration:
+    buffer.write(newline)
+    buffer.write(utf8_bytes('DWORD %s[%s] = {' % (variable, total)))
+
+    # emit pixeldata:
+    linepixels = 0
+    for y in range(height):
+        for x in range(width):
+
+            if linepixels % 5 == 0:
+                buffer.write(newline)
+                buffer.write(b'    ')
+                linepixels = 0
+
+            linepixels += 1
+
+            # output 0xARGB order with premultiplied alpha:
+            R, G, B, A = pixels[x, y]
+            buffer.write(b'0x' + BYTES_RGB_HEX[A]
+                               + BYTES_RGB_HEX[int((A * R) / 0xFF)]
+                               + BYTES_RGB_HEX[int((A * G) / 0xFF)]
+                               + BYTES_RGB_HEX[int((A * B) / 0xFF)]
+                               + b',')
+
+    # finish the declaration:
+    buffer.write(newline)
+    buffer.write(b'};')
+    buffer.write(newline)
+
+
+def compile_image(image, descriptor, variable, newline, buffering):
+    """
+    A wrapper over dibits2h() that uses an internal buffer to hold the result
+    when 'buffering' is True. Otherwise it writes directly to the given descriptor.
+    """
+    if buffering:
+        buffer = io.BytesIO()
+        dibits2h(image, buffer, variable, newline)
+        descriptor.write(buffer.getvalue())
+
+    else:
+        dibits2h(image, descriptor, variable, newline)
+
+
+# Parser:
+
+def make_parser():
+    parser = ArgumentParser(
+        description = __doc__,
+        formatter_class = RawDescriptionHelpFormatter,
+        epilog = 'example: EmbedDIBits.py box.png wall.png --stdout > sprites.h',
+        usage  = 'EmbedDIBits.py image [image ...] [option [options ...]]')
+
+    # positional:
+    parser.add_argument('filepaths',
+        help = 'image to convert to a C header file',
+        metavar = 'filepath', nargs = '+')
+
+    # optional:
+    parser.add_argument('--newline',
+        help = 'use a specific newline mode (default: system)',
+        choices = ['dos', 'mac', 'unix', 'system'], default = 'system')
+
+    parser.add_argument('--no-buffer',
+        help = 'write directly to files (slower, uses less memory)',
+        action = 'store_true')
+
+    parser.add_argument('--quiet',
+        help = 'do not print file -> target information',
+        action = 'store_true')
+
+    parser.add_argument('--stdout',
+        help = 'print code to standard output instead of files',
+        action = 'store_true')
+
+    return parser
+
+
+# Entry point:
+
+def main():
+    parser = make_parser()
+    options = parser.parse_args()
+    status = 0
+
+    buffering = (options.no_buffer == False)
+    newline = BYTES_NEWLINES[options.newline]
+
+    for filepath in options.filepaths:
+        try:
+            image = Image.open(filepath)
+            variable = noext_basename(filepath)
+
+            # compile to stdout:
+            if options.stdout:
+                compile_image(image, sys.stdout.buffer, variable, newline, buffering)
+
+            # compile to a C header file:
+            else:
+                target = noext(filepath) + '.h'
+
+                if not options.quiet:
+                    outln('%s -> %s' % (filepath, target))
+
+                with open(target, 'wb') as descriptor:
+                    compile_image(image, descriptor, variable, newline, buffering)
+
+        # note that this does not handle KeyboardInterrupt
+        # since it is derived from BaseException:
+        except Exception as err:
+            errln('error: %s - %s' % (filepath, str(err)))
+            status = 1
+
+    sys.exit(status)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+
